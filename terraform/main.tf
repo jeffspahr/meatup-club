57ccad877a06667a7009aaeabb1388870f554ec1
Jeff Spahr <spahrj@gmail.com>
2026-04-29 19:51:42 -0400

    Remove unused Terraform quota config
    Signed-off-by: Jeff Spahr <spahrj@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 5.15"
     }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 7.30"
-    }
   }
 }
 
